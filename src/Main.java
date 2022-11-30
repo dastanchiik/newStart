a5959b476b06fdecb870efdf7fa7c544f07a7875
dastanchick <dosyafree32@gmail.com>
2022-11-30 15:08:08 +0600

    I added all

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,89 @@
 import java.util.Random;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("-----<<<Start game>>>-----" + "\n" + "Predicate amount of points (2 ... 12)" + "\n" + "Write here:");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int count = 1;
+        if (number > 1 && number < 13) {
+            while (count != 4) {
+                game(number);
+                count++;
+                if (count != 4) {
+                    System.out.println("-----<<<Round " + count + ">>>-----" + "\n" + "Predicate amount of points (2 ... 12)" + "\n" + "Write here:");
+                    number = scanner.nextInt();
+                }
+            }
+        }
+    }
+    public static void game(int a) {
         Random random = new Random();
-        int i = random.nextInt(50,150);
-        int num = random.nextInt(50,150);
-        int number = random.nextInt(50,150);
-        int number1 = random.nextInt(50,150);
-        int number2 = random.nextInt(50,150);
-        int number3 = random.nextInt(50,150);
-        int a = Math.max(i,num);
-        int b = Math.max(number,number1);
-        int c = Math.max(number2,number3);
-        int d = Math.max(a,b);
-        int e = Math.max(c,d);
-        System.out.println(i+"\n"+num+"\n"+number1+"\n"+number2+"\n"+number3+"\n"+number);
-        System.out.println("max num: "+e);
+        int ran = random.nextInt(1, 6);
+        int ran0 = random.nextInt(1, 6);
+        cubUser1(ran);
+        cubUser1(ran0);
+        int plus = ran + ran0;
+        int b = Math.max(plus, a);
+        int c = Math.min(plus, a);
+        int d = b - c;
+        int p = d * 2;
+        int botNumber = random.nextInt(2, 12);
+        System.out.println("Number bot: " + botNumber);
+        int superPlus = random.nextInt(1, 6);
+        int superMinus = random.nextInt(1, 6);
+        cubUser1(superPlus);
+        cubUser1(superMinus);
+        int megaPlus = superPlus + superMinus;
+        int q = Math.max(megaPlus, botNumber);
+        int w = Math.min(megaPlus, botNumber);
+        int e = q - w;
+        int o = e * 2;
+        System.out.println("-----Current score-----");
+        System.out.println("point user: " + p + "\n" + "point bot: " + o);
+        int countUser = 0;
+        int countBot = 0;
+        int countDraw = 0;
+        if (p > o) {
+            int num = p - o;
+            System.out.println("User is ahead by " + num + " points!");
+            countUser++;
+        } else if (p == o) {
+            System.out.println("Draw");
+            countDraw++;
+        } else {
+            int num1 = p - o;
+            System.out.println("Bot is ahead by " + num1 + " points!");
+            countBot++;
+        }
+        if (countBot > countUser && countDraw < countBot) {
+            System.out.println("Bot win");
+        } else if (countUser > countBot && countUser > countDraw) {
+            System.out.println("User win");
+        } else {
+            System.out.println("Draw");
+        }
+    }
+    public static void cubUser1(int randomForCub) {
+        switch (randomForCub) {
+            case 1:
+                System.out.println("+-------+\n|       |\n|   #   |\n|       |\n+-------+");
+                break;
+            case 2:
+                System.out.println("+-------+\n| #     |\n|       |\n|     # |\n+-------+");
+                break;
+            case 3:
+                System.out.println("+-------+\n|     # |\n|   #   |\n| #     |\n+-------+");
+                break;
+            case 4:
+                System.out.println("+-------+\n| # #   |\n| # #   |\n|       |\n+-------+");
+                break;
+            case 5:
+                System.out.println("+-------+\n| #   # |\n|   #   |\n| #   # |\n+-------+");
+                break;
+            case 6:
+                System.out.println("+-------+\n| #   # |\n| #   # |\n| #   # |\n+-------+");
+                break;
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//        Random random = new Random();
-//        int number = random.nextInt();
-//        int number1 = random.nextInt();
-//        int number2 = random.nextInt();
-//        int number3 = random.nextInt();
-//        int number4 = random.nextInt();
-//        int number5 = random.nextInt();
-//        boolean when = number<number1;
-//        boolean how = when = true;
