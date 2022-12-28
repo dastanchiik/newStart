9f2c161716046df08dcdb215e09aad51c4327a94
dastanchick <dosyafree32@gmail.com>
2022-12-28 17:31:18 +0600

    It's wrong

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,29 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         method();
     }
     public static void method(){
-        ArrayList<Integer>ints = new ArrayList<>();
-        ArrayList<Integer>tak = new ArrayList<>();
-        ArrayList<Integer>jup = new ArrayList<>();
-        int[]array = {30, 49, 74, 9, 5, 60, 62, 35, 93, 16, 84, 73, 48, 93, 85, 3, 63, 90, 58, 99, 15, 28, 98, 2, 89, 51, 18, 6, 52, 21, 87, 20, 31, 97, 78, 58, 3, 86, 55, 88, 57, 26, 27, 45, 2, 21, 85, 97, 70, 74};
-        for (Integer e:array) {
-            ints.add(e);
-            if (e%2==0){
-                jup.add(e);
-            } else if (e%2==1) {
-                tak.add(e);
-            }
+       ArrayList<Integer>arr = new ArrayList<>();
+       Random ran = new Random();
+       int count = 0;
+       int counter = 0;
+       int plus = 0;
+       while (count<=400){
+           int randomZero = ran.nextInt(100);
+           arr.add(randomZero);
+           count++;
+       }
+        for (int i = 0; i <= arr.size(); i++) {
+        int p = arr.get(plus);
+        counter += p;
         }
-        System.out.println("all number:"+ints);
-        System.out.println("tak sandar:"+tak);
-        System.out.println("jup sandar:"+jup);
+        Set<Integer>set = new HashSet<>();
+        set.add(counter);
+        System.out.println(set);
     }
 }
